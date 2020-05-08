7f893f1ceeb79edb250a9624ef10a054faf1e04a
Jaziel Lopez <juan.jaziel@gmail.com>
2020-05-07 21:03:00 -0700

    Update linear.py

--- linear.py
+++ linear.py
@@ -1,4 +1,4 @@
-import string
+from src.linear import get_varname_from, rslv_linear_equation
 import os
 import colorama
 
@@ -16,35 +16,12 @@ print("                     CTRL^C or CTRL^D to terminate program")
 print("---")
 
 while True:
+
     equality = input(colorama.Fore.YELLOW + " > ").replace(" ", "")
-    UNK = list(filter(lambda x: x in equality, string.ascii_letters))[0]
 
-    # tokenize
-    eq_chunks = equality.split("=")
-    rgt_sd_equal = eq_chunks[0]
-    lft_sd_equal = int(eq_chunks[1])
-    mst_lft_side = 1
+    o_variable_name = get_varname_from(equality)
 
-    # tokenize operators
-    if "+" in rgt_sd_equal:
-        chunks = rgt_sd_equal.split("+")
-        for chunk in chunks:
-            if chunk.isdigit():
-                lft_sd_equal = lft_sd_equal - float(chunk)
-            else:
-                try:
-                    mst_lft_side = float(chunk.rstrip(UNK))
-                except:
-                    pass
-    else:
-        chunks = rgt_sd_equal.split("-")
-        for chunk in chunks:
-            if chunk.isdigit():
-                lft_sd_equal = lft_sd_equal + float(chunk)
-            else:
-                try:
-                    mst_lft_side = float(chunk.rstrip(UNK))
-                except:
-                    pass
+    result = rslv_linear_equation(equality)
 
-    print(colorama.Fore.GREEN + "\t%s = %1.f" % (UNK, lft_sd_equal / mst_lft_side))
+    print(colorama.Fore.GREEN + "\t%s = %.2f" % (o_variable_name, result))
+
