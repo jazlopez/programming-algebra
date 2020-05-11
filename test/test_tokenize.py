273b64773c753bf6d279bc1424d814ca77d68421
Jaziel Lopez <juan.jaziel@gmail.com>
2020-05-11 16:33:48 -0700

    Add files via upload

--- test/test_tokenize.py
+++ test/test_tokenize.py
@@ -9,6 +9,7 @@ class TestTokenizeCase(unittest.TestCase):
     def test_tokenize(self):
         self.assertEqual(tokenize("121+1+30*45"), ["121", "+", "1", "+", "30", "*", "45"])
         self.assertEqual(tokenize("2+6.00"), ["2", "+", "6.00"])
+        self.assertEqual(tokenize("2x+3+2"), ["2x", "+", "3", "+", "2"])
 
 
 if __name__ == '__main__':
